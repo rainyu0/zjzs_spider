2fbea7d7197c64fbdfcffd336bd856f634030283
shengyu.yusy <shengyu.yusy@alibaba-inc.com>
2018-12-24 22:17:47 +0800

    first page ok

--- zjzs_spider/pipelines.py
+++ zjzs_spider/pipelines.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 
+import codecs
+import json
+
 # Define your item pipelines here
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
@@ -7,5 +10,11 @@
 
 
 class ZjzsSpiderPipeline(object):
+    def __init__(self):
+        self.file = codecs.open('data_cn.json', 'wb', encoding='utf-8')
+
     def process_item(self, item, spider):
+        line = json.dumps(dict(item)) + '\n'
+        self.file.write(line.decode("unicode_escape"))
         return item
+
--- zjzs_spider/spiders/subject_spider.py
+++ zjzs_spider/spiders/subject_spider.py
@@ -28,20 +28,34 @@ class SubjectSpider(scrapy.Spider):
         'http://zt.zjzs.net/xk2020/area_0_0.html'
     ]
 
-    def start_requests(self):
-        urls = [
-                'http://zt.zjzs.net/xk2020/area_0_0.html'
-            ]
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
-
     def parse(self, response):
-        all_area = response.xpath('//table/tr/td/a')
-        print all_area
-        outfile = open('out.txt', 'w')
+        all_area = response.xpath('//table/tr')
         for one_area in all_area:
-            item = one_area.extract().encode('utf-8')
-            outfile.write(item)
-            outfile.write('\n')
-        outfile.close()
+            if len(one_area.css('td')) == 5 :
+                if len(one_area.xpath('td')[3].xpath('a/@href').extract()) > 0 :
+                    yield {
+                        'area': one_area.xpath('td')[0].extract()[4:-5],
+                        'school_no': one_area.xpath('td')[1].extract()[4:-5],
+                        'school_name': one_area.xpath('td')[2].extract()[4:-5],
+                        'school_url': one_area.xpath('td')[3].xpath('a/@href').extract_first(),
+                        'inner_url': one_area.xpath('td')[4].xpath('a/@href').extract_first()
+                    }
+
+
+    # def start_requests(self):
+    #     urls = [
+    #             'http://zt.zjzs.net/xk2020/area_0_0.html'
+    #         ]
+    #     for url in urls:
+    #         yield scrapy.Request(url=url, callback=self.parse)
+
+    # def parse(self, response):
+    #     all_area = response.xpath('//table/tr/td/a')
+    #     print all_area
+    #     outfile = open('out.txt', 'w')
+    #     for one_area in all_area:
+    #         item = one_area.extract().encode('utf-8')
+    #         outfile.write(item)
+    #         outfile.write('\n')
+    #     outfile.close()
 
